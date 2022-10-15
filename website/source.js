1918cb142ffe73539092b26e6697ce1e347d763f
Tanakorn <oozoonteenoo1234@gmail.com>
2022-10-15 18:29:07 +0700

    Update git .

--- website/source.js
+++ website/source.js
@@ -1,42 +1,52 @@
 const genBtn = document.getElementById('Generate');
 var prompt = document.getElementById("prompt");
+const progress = document.getElementById('pg');
+const artist = document.getElementById('artist')
+const style = document.getElementById('style1')
+const model = document.getElementById('model')
+
 
 
 async function generateImage() {
+console.log(artist.value, style.value, model.value)
+const encodedParams = new URLSearchParams();
+encodedParams.append("prompt", `${prompt.value},${style.value},made by ${artist.value}`);
+encodedParams.append("guidance", "10");
+encodedParams.append("width", "512");
+encodedParams.append("sampler", model.value);
+encodedParams.append("height", "512");
+encodedParams.append("steps", "50");
+console.log(`${prompt.value},${style.value},made by ${artist.value}`)
 
-	
+const options = {
+	method: 'POST',
+	url: 'https://dezgo.p.rapidapi.com/text2image',
+	headers: {
+		'content-type': 'application/x-www-form-urlencoded',
+		'X-RapidAPI-Key': '56075dc1cdmshdd2da87524546fbp122840jsn54a3136b483d',
+		'X-RapidAPI-Host': 'dezgo.p.rapidapi.com'
+	},
+	body: encodedParams
+};
 
-	const encodedParams = new URLSearchParams();
-	encodedParams.append("prompt", prompt);
-	encodedParams.append("guidance", "7.5");
-	encodedParams.append("width", "512");
-	encodedParams.append("sampler", "k_lms");
-	encodedParams.append("height", "512");
-	encodedParams.append("steps", "50");
+var response = await fetch('https://dezgo.p.rapidapi.com/text2image', options)
+var pngBlob = await response.blob();    
 
-	const options = {
-		method: 'POST',
-		url: 'https://dezgo.p.rapidapi.com/text2image',
-		headers: {
-			'content-type': 'application/x-www-form-urlencoded',
-			'X-RapidAPI-Key': '56075dc1cdmshdd2da87524546fbp122840jsn54a3136b483d',
-			'X-RapidAPI-Host': 'dezgo.p.rapidapi.com'
-		},
-		body: encodedParams
-	};
-
-	var response = await fetch('https://dezgo.p.rapidapi.com/text2image', options)
-	var pngBlob = await response.blob();    
-	
-	console.log("Got the image as a blob:", pngBlob)
-
-	document.getElementById("image").src = URL.createObjectURL(pngBlob);
+prompt.value = ''
+document.getElementById("image").src = URL.createObjectURL(pngBlob);
+progress.classList.remove('animation')
 }
 
 genBtn.addEventListener('click', (e)=>{
-	e.preventDefault()
-	if(prompt.value){
-		generateImage()
-	}
+e.preventDefault()
+
+genBtn.disabled = true
+if(prompt.value){
+	progress.classList.add('animation')
+	generateImage()
+}
+genBtn.disabled = false
+
+
 })
 
